f82e12117ad45cc7e8db513e9f3b140489f3c9b6
maxthnd <schule.deckermaximilian@gmx.de>
2024-11-03 02:09:02 +0100

    Frontend-API Call fixes

--- src/router/index.ts
+++ src/router/index.ts
@@ -89,7 +89,7 @@ const router = createRouter({
   ]
 });
 
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'http://164.92.187.212:8080';
 
 function isTokenExpired(token: string): boolean {
   if (!token || token.split('.').length !== 3) {
