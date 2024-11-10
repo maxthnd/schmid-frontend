1d013b8222b9c3aa95f06db88de4301d746e2fab
maxthnd <schule.deckermaximilian@gmx.de>
2024-11-10 19:14:04 +0100

    router fallback fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
@@ -15,4 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
